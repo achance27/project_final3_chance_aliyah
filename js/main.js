6e428784146f6634372547c0afa154f0128c41a8
aliyahchance <aliyahachance@gmail.com>
2018-04-12 19:49:14 -0400

    Made Hot Cold header change once

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
+// 2 jQuery Plugins
 // Menu Page Tabs & Accordian
 $('#responsiveTabsDemo').responsiveTabs({
     startCollapsed: 'accordion'
 });
 
+// Custom JavaScript
 /*document.getElementsByTagName('h3')[0].addEventListener('mouseover', function () {
     var drinks = document.createElement('p');
 		drinks.innerHTML = 'You can have whatever you like';
     document.getElementsByTagName('h3')[0].appendChild(drinks);
 });
 */
+
+// Custom jQuery
 //Fade out Hot and fade in words "or Cold?" and then fade in "Hot or Cold?"
+var i = 1;
 $('#hot').mouseover(function () {
-    $('h3').fadeIn('xslow').html('Or Cold?');
+	if (i == 1){
+		$('h3').fadeIn('90000').html('Or Cold?');
+	}
+	i = 2;
+
 });
 $('#hot').mouseout(function () {
-    $('h3').fadeIn('xslow').html('Hot Or Cold?');
+    $('h3').fadeIn('90000').html('Hot or Cold?');
 });
