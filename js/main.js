29c93e4c1ce3f6841fbe6f68cc9d6624fefde219
aliyahchance <aliyahachance@gmail.com>
2018-04-13 18:48:48 -0400

    Add (rough) intro page

--- js/main.js
+++ js/main.js
@@ -26,39 +26,29 @@ $('#myBlock').vide('extended path as a string', 'options as a string'
 document.body.className += 'fade-out';
 $(function() {
     $('body').removeClass('fade-out');
-});*/
-/*
-$('div').className += 'fade-out';
-$(function () {
-    $('div').removeClass('fade-out');
-}); */
-$(document).ready(function() {
-  $(".animsition").animsition({
-    inClass: 'flip-in-y-fr',
-    outClass: 'flip-out-y-fr',
-    inDuration: 1500,
-    outDuration: 800,
-    linkElement: '.animsition-link',
-    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
-    loading: true,
-    loadingParentElement: 'body', //animsition wrapper element
-    loadingClass: 'animsition-loading',
-    loadingInner: '', // e.g '<img src="loading.svg" />'
-    timeout: false,
-    timeoutCountdown: 5000,
-    onLoadEvent: true,
-    browser: [ 'animation-duration', '-webkit-animation-duration'],
-    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-    overlay : false,
-    overlayClass : 'animsition-overlay-slide',
-    overlayParentElement : 'body',
-    transition: function(url){ window.location.href = url; }
-  });
 });
 
+$(document).ready(function(){
+    $('.background-video').bgVideo();
+});
+*/
+
+$(".rotate").textrotator({
+  animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+  separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+  speed: 2000 // How many milliseconds until the next word show.
+});
+
+
+// Intro Background Video
+$('.jquery-background-video').bgVideo( {
+    fullScreen: true,
+    fadeIn: 2000,
+});
+
+
 // Menu Page Tabs & Accordian
-$('#responsiveTabsDemo').responsiveTabs({
+$('#responsiveTabsDemo').responsiveTabs( {
     startCollapsed: 'accordion'
 });
 
@@ -69,6 +59,11 @@ $('#responsiveTabsDemo').responsiveTabs({
     document.getElementsByTagName('h3')[0].appendChild(drinks);
 });
 */
+
+// To attach Backstrech as the body's background
+// $.backstretch('img/coffee-clip.mp4');
+
+
 
 // Custom jQuery
 //Fade out Hot and fade in words "or Cold?" and then fade in "Hot or Cold?"
@@ -82,4 +77,10 @@ $('#hot').mouseover(function () {
 });
 $('#hot').mouseout(function () {
     $('h3').fadeIn('90000').html('Hot or Cold?');
+});
+
+// Container Fade Transition
+$('div').className += 'fade-out';
+$(function () {
+    $('div').removeClass('fade-out');
 });
